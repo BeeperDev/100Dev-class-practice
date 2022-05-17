f11d3bc43ae2ab939baad97f80d3054ae424997b
Erica Layton <e.alexislayton@gmail.com>
2022-05-16 17:23:26 -0700

    Add solutions to JS super review problems

---
+++ class34.5-super-review/variable-fun-time/js/main.js
@@ -0,0 +1,31 @@
+//--- Easy
+//create a variable and assign it a number
+let aNum = 2
+
+
+//minus 10 from that number
+aNum -= -10
+
+//print that number to the console
+console.log(aNum)
+
+//--- Medium
+//create a variable that holds a value from the input
+let inputVal = Number(document.querySelector('input').value)
+
+//add 25 to that number
+inputVal += 25
+
+//alert that number
+alert(inputVal)
+
+//--- Hard
+//create a variable that holds the h1
+let h1Var = document.querySelector('h1')
+
+//add an event listener to that element that console logs the sum of the two previous variables
+h1Var.addEventListener(click, addTwo)
+
+function addTwo(){
+    console.log(aNum+inputVal)
+}
