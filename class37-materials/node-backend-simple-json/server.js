0d7107414ef9d86ca8d25bc14f7941a192f4a3ce
Erica Layton <e.alexislayton@gmail.com>
2022-05-29 14:47:20 -0700

    Add notes to class37 practice problems

--- class37-materials/node-backend-simple-json/server.js
+++ class37-materials/node-backend-simple-json/server.js
@@ -1,19 +1,18 @@
 // Modules that have been written for us that do the heavy lifting
 
-// 
 const http = require('http');                 // Comes with everything we need to create a server, listen to network requests and respond to them
 const fs = require('fs')                      // Comes with everything we need to access the disk and grab the files we need
-const url = require('url');                   // Look at the url that we need to make requests
+const url = require('url');                   // Look at the url and path that we need to make requests
 const querystring = require('querystring');   // Look at the query parameters for the request
 const figlet = require('figlet')              // Takes any string and turns it into ASCII art (404)
 
 // We set up our server to listen for specific things.
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {                // Using the http module to create the server
   const page = url.parse(req.url).pathname;                     // gets the url path
   const params = querystring.parse(url.parse(req.url).query);   // gets the query parameters
   console.log(page);
-  if (page == '/') {
+  if (page == '/') {                                        
     fs.readFile('index.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});    // 200 means everything is ok, we're going to send you some html
       res.write(data);                                      // We're gonna pull that html
@@ -35,7 +34,7 @@ const server = http.createServer((req, res) => {
     });
   }
   else if (page == '/api') {
-    if('student' in params){
+    if('student' in params){                          // Was there a student query Param?
       if(params['student']== 'leon'){
         res.writeHead(200, {'Content-Type': 'application/json'});
         let flipRes = Math.ceil(Math.random()*2) === 1 ? 'heads' : 'tails'
